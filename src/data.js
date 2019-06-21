b0d56368150a0ef7de21883df4b5b10751597def
rayt90 <rayt90@users.noreply.github.com>
2019-06-22 00:26:20 +0800

    Added styled-componets. Created data file and added 4 books.

---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const storeProducts = [
+  /*{
+    id:,
+    title: "",
+    img: "",
+    price: ,
+    author: "",
+    description: "",
+    genre: "",
+    inCart: false,
+    count: 0,
+    total: 0
+  },*/
+  {id: 1,
+  title: "Good Girl, Bad Girl",
+  img: "img/good-girl-bad-girl.jpg",
+  price: 20,
+  author: "Michael Robotham",
+  description: "The girl with no past.Six years ago, Evie Cormac was discovered, filthy and half-starved, hiding in a secret room in the aftermath of a shocking crime. Now approaching adulthood, Evie is damaged, self-destructive and has never revealed her true identity.The boy who survived.Forensic psychologist Cyrus Haven, a man haunted by his own past, is investigating the death of champion figure-skater Jodie Sheehan. When Cyrus is called upon to assess Evie, she threatens to disrupt the case and destroy his ordered life. Because Evie has a unique and dangerous gift - she knows when someone is lying. And nobody is telling the truth.Good Girl, Bad Girl is an unnerving psychological thriller from one of the greatest crime writers of today, Michael Robotham, bestselling author of The Other Wife and The Secrets She Keeps.",
+  genre: "crime",
+  inCart: false,
+  count: 0,
+  total: 0},
+  {
+    id:2,
+    title: "It",
+    img: "img/it.jpg",
+    price: 15,
+    author: "Stephen king",
+    description: "To the children, the town was their whole world. To the adults, knowing better, Derry Maine was just their home town: familiar, well-ordered for the most part. A good place to live.It is the children who see - and feel - what makes the small town of Derry so horribly different. In the storm drains, in the sewers, IT lurks, taking on the shape of every nightmare, each one's deepest dread. Sometimes IT reaches up, seizing, tearing, killing . . .Time passes and the children grow up, move away and forget. Until they are called back, once more to confront IT as IT stirs and coils in the sullen depths of their memories, reaching up again to make their past nightmares a terrible present reality.",
+    genre: "horror",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id:3,
+    title: "Misery",
+    img: "img/misery.jpg",
+    price: 15,
+    author: "Stephen king",
+    description: "Misery Chastain was dead. Paul Sheldon had just killed her - with relief, with joy. Misery had made him rich; she was the heroine of a string of bestsellers. And now he wanted to get on to some real writing.That's when the car accident happened, and he woke up in pain in a strange bed. But it wasn't the hospital. Annie Wilkes had pulled him from the wreck, brought him to her remote mountain home, splinted and set his mangled legs.The good news was that Annie was a nurse and has pain-killing drugs. The bad news was that she was Paul's Number One Fan. And when she found out what Paul had done to Misery, she didn't like it. She didn't like it at all. And now he had to bring Misery back to life. Or else . . . .",
+    genre: "horror",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id:3,
+    title: "The Scholar",
+    img: "img/the-scholar.jpg",
+    price: 25,
+    author: "Cormac Reilly",
+    description: "Being brilliant has never been this dangerous ...When Dr Emma Sweeney stumbles across the victim of a hit and run outside Galway University late one evening, she calls her partner, Detective Cormac Reilly, bringing him first to the scene of a murder that would otherwise never have been assigned to him.A security card in the dead woman's pocket identifies her as Carline Darcy, a gifted student and heir apparent to Irish pharmaceutical giant Darcy Therapeutics. The multi-billion-dollar company, founded by her grandfather, has a finger in every pie, from sponsoring university research facilities to funding political parties to philanthropy - it has funded Emma's own ground-breaking research. The enquiry into Carline's death promises to be high profile and high pressure.As Cormac investigates, evidence mounts that the death is linked to a Darcy laboratory and, increasingly, to Emma herself. Cormac is sure she couldn't be involved, but as his running of the case comes under scrutiny from the department and his colleagues, he is forced to question his own objectivity. Could his loyalty to Emma have led him to overlook evidence? Has it made him a liability? ",
+    genre: "crime",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id:4,
+    title: "Children of Ruin",
+    img: "img/children-of-ruin.jpg",
+    price: 20,
+    author: "Adrian Tchaikovsky",
+    description: "It has been waiting through the ages.Now it's time...Thousands of years ago, Earth's terraforming program took to the stars. On the world they called Nod, scientists discovered alien life - but it was their mission to overwrite it with the memory of Earth. Then humanity's great empire fell, and the program's decisions were lost to time.Aeons later, humanity and its new spider allies detected fragmentary radio signals between the stars. They dispatched an exploration vessel, hoping to find cousins from old Earth.But those ancient terraformers woke something on Nod better left undisturbed.And it's been waiting for them.Children of Ruin follows the author's Children of Time, winner of the Arthur C. Clarke award. It is set in the same universe, with a new cast of characters.",
+    genre: "Science Fiction",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+]
